341914565b422077a33aa2adaceabfd003e6317e
Rizal Agatha Erdin Agesyah <rizalagatha1403.com>
2025-04-18 03:44:25 +0700

    Backend Test Nexa

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.use(express.json());
 app.use('/api', authRoutes);
 app.use('/api/karyawan', karyawanRoutes);
 
+process.on('SIGTERM', () => {
+  console.log('SIGTERM received, shutting down gracefully');
+  process.exit(0);
+});
+
+
 app.listen(3000, () => {
   console.log('Server berjalan di port 3000');
 });
